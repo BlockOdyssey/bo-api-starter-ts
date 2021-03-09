5f82f1f25d8286e7c02a3fb36a2ba98c377064c8
blockodyssey-whlee <dokdo2005@blockodyssey.io>
2021-03-09 11:07:09 +0900

    index.ts 수정

--- default/index.ts
+++ default/index.ts
@@ -8,16 +8,16 @@ import * as webMw from "./middleware/webMw";
 import * as appMw from "./middleware/appMw";
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 
 app.use("/web", webMw.default);
-for (let k in webRoutes) {
-  app.use("/web/" + k, webRoutes[k]);
+for (let k in webRoutes.default) {
+  app.use("/web/" + k, webRoutes.default[k]);
 }
 
 app.use("/app", appMw.default);
-for (let k in appRoutes) {
-  app.use("/app/" + k, appRoutes[k]);
+for (let k in appRoutes.default) {
+  app.use("/app/" + k, appRoutes.default[k]);
 }
 
 app.get("/", (req: express.Request, res: express.Response) => {
