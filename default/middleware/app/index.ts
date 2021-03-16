93e5550f00c41320b026f467985262b2e89a10f2
blockodyssey-whlee <dokdo2005@blockodyssey.io>
2021-03-16 14:30:19 +0900

    앱 미들웨어 문구 수정

--- default/middleware/app/index.ts
+++ default/middleware/app/index.ts
@@ -3,23 +3,23 @@ import * as express from "express";
 // middle-ware settings
 
 const appMw = (
-  req: express.Request,
-  res: express.Response,
-  next: express.NextFunction
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
 ) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Content-Length, Authorization, Origin, Accept, X-Requested-With, user_idx, token"
-  );
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Content-Type, Content-Length, Authorization, Origin, Accept, X-Requested-With, user_idx, token"
+    );
+    res.header(
+        "Access-Control-Allow-Methods",
+        "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    );
 
-  console.log("web middleware ---");
+    console.log("app middleware ---");
 
-  next();
+    next();
 };
 
 module.exports = appMw;
