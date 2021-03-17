8bf56a735315b0407ef7eaca02320e6246ff27f7
blockodyssey-whlee <dokdo2005@blockodyssey.io>
2021-03-17 14:08:16 +0900

    findUser 에러 핸들링 추가

--- default/routes/web/users.ts
+++ default/routes/web/users.ts
@@ -5,13 +5,16 @@ const router = express.Router();
 
 router.get("/", async (req: express.Request, res: express.Response) => {
     const { user_idx } = res.locals.decoded;
-    const result = await users.findUser({ user_idx });
-    console.log(result);
-
-    if (result) {
-        res.status(200).json({ ...statusMessages.ok, dataSource: result });
-    } else {
-        res.status(404).json(statusMessages.notFound);
+    try {
+        const result = await users.findUser({ user_idx });
+        if (result) {
+            console.log(result);
+            res.status(200).send({ ...statusMessages.ok, dataSource: result });
+        } else {
+            res.status(404).send(statusMessages.notFound);
+        }
+    } catch (err) {
+        res.status(500).send(statusMessages.internalError);
     }
 });
 
--- default/service/users/findUser.ts
+++ default/service/users/findUser.ts
@@ -1,19 +1,13 @@
 const { find } = require("../../query/users");
 
-interface user {
-  email: string;
+interface userData {
+    user_idx: number;
 }
 
-exports.findUser = async (obj: user) => {
-  console.log("service findUser :: ", obj);
+exports.findUser = async (obj: userData) => {
+    console.log("service findUser :: ", obj);
 
-  // 로직 작성은 여기서
-  if (obj.email) {
-    console.log("email로 조회하기");
-  }
-
-  // 쿼리 실행
-  const user = await find(obj);
-
-  return user;
+    // 쿼리 실행
+    const user = await find(obj);
+    return user;
 };
