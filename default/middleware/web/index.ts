abad70ef7c268df10a4acb9ee3828f9d556c885d
blockodyssey-whlee <dokdo2005@blockodyssey.io>
2021-03-17 15:16:55 +0900

    URL에 따라 토큰 인증 여부를 달리하도록 작성

--- default/middleware/web/index.ts
+++ default/middleware/web/index.ts
@@ -9,6 +9,8 @@ interface decodedToken {
     iat: number;
 }
 
+const tokenCheckUrl: string[] = ["/upload", "/users"];
+
 // middle-ware settings
 const webMw = (
     req: express.Request,
@@ -27,45 +29,58 @@ const webMw = (
 
     console.log("web middleware ---");
 
-    const token = req.get("token");
-    jwt.verify(
-        token,
-        secret,
-        (err: jwt.VerifyErrors, decoded: decodedToken) => {
-            if (
-                err ||
-                !decoded.user_idx ||
-                decoded.user_idx === null ||
-                isNaN(decoded.user_idx)
-            ) {
-                res.status(401).send(statusMessages.unauthorized);
-            } else {
-                if (req.body) {
-                    for (let i in req.body) {
-                        if (
-                            req.body[i] === undefined ||
-                            req.body[i] === null ||
-                            req.body[i] === ""
-                        ) {
-                            res.status(400).send(statusMessages.badRequest);
-                        }
-                    }
-                } else if (req.query) {
-                    for (let i in req.query) {
-                        if (
-                            req.query[i] === undefined ||
-                            req.query[i] === null ||
-                            req.query[i] === ""
-                        ) {
-                            res.status(400).send(statusMessages.badRequest);
-                        }
-                    }
+    const checkParams = () => {
+        if (req.body) {
+            for (let i in req.body) {
+                if (
+                    req.body[i] === undefined ||
+                    req.body[i] === null ||
+                    req.body[i] === ""
+                ) {
+                    res.status(400).send(statusMessages.badRequest);
                 }
-                res.locals.decoded = decoded;
-                next();
+            }
+        } else if (req.query) {
+            for (let i in req.query) {
+                if (
+                    req.query[i] === undefined ||
+                    req.query[i] === null ||
+                    req.query[i] === ""
+                ) {
+                    res.status(400).send(statusMessages.badRequest);
+                }
             }
         }
-    );
+    };
+
+    if (tokenCheckUrl.includes(req.url)) {
+        const token: string = req.get("token");
+        if (token) {
+            jwt.verify(
+                token,
+                secret,
+                (err: jwt.VerifyErrors, decoded: decodedToken) => {
+                    if (
+                        err ||
+                        !decoded.user_idx ||
+                        decoded.user_idx === null ||
+                        isNaN(decoded.user_idx)
+                    ) {
+                        res.status(401).send(statusMessages.unauthorized);
+                    } else {
+                        checkParams();
+                        res.locals.decoded = decoded;
+                        next();
+                    }
+                }
+            );
+        } else {
+            res.status(401).send(statusMessages.unauthorized);
+        }
+    } else {
+        checkParams();
+        next();
+    }
 };
 
 module.exports = webMw;
