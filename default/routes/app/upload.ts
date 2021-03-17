45a161d62aa981e4934009e09e0debb0cb90bec2
blockodyssey-whlee <dokdo2005@blockodyssey.io>
2021-03-17 15:37:20 +0900

    파일 업로드 로직 변경

--- default/routes/app/upload.ts
+++ default/routes/app/upload.ts
@@ -3,19 +3,22 @@ const statusMessages = require("../../statusMessages");
 const uploadFile = require("../../middleware/common/uploadFile");
 const router = express.Router();
 
-router.post("/", async (req: express.Request, res: express.Response) => {
-    const { user_idx } = res.locals.decoded;
-    try {
-        await uploadFile.single("file");
-        res.status(201).send({
-            ...statusMessages.created,
-            dataSource: {
-                url: req.file.path,
-            },
-        });
-    } catch (err) {
-        res.status(500).send(statusMessages.internalError);
+router.post(
+    "/",
+    uploadFile.single("file"),
+    (req: express.Request, res: express.Response) => {
+        const { user_idx } = res.locals.decoded;
+        try {
+            res.status(201).send({
+                ...statusMessages.created,
+                dataSource: {
+                    url: req.file.path,
+                },
+            });
+        } catch (err) {
+            res.status(500).send(statusMessages.internalError);
+        }
     }
-});
+);
 
 module.exports = router;
