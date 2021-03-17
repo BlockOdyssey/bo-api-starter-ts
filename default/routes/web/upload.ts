2fe1999038eb1d84f54f9b603cd05dc02c321371
blockodyssey-whlee <dokdo2005@blockodyssey.io>
2021-03-17 16:00:53 +0900

    파일 업로드 시 body가 제대로 들어오지 않는 경우 핸들링 추가

--- default/routes/web/upload.ts
+++ default/routes/web/upload.ts
@@ -8,15 +8,15 @@ router.post(
     uploadFile.single("file"),
     (req: express.Request, res: express.Response) => {
         const { user_idx } = res.locals.decoded;
-        try {
+        if (req.file) {
             res.status(201).send({
                 ...statusMessages.created,
                 dataSource: {
                     url: req.file.path,
                 },
             });
-        } catch (err) {
-            res.status(500).send(statusMessages.internalError);
+        } else {
+            res.status(400).send(statusMessages.badRequest);
         }
     }
 );
