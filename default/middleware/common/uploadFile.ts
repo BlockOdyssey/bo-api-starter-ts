076540057a94cf159414e8f8c901cc7644351ac3
blockodyssey-whlee <dokdo2005@blockodyssey.io>
2021-03-17 09:26:44 +0900

    s3에서 파일 확장자를 같이 리턴하도록 수정

--- default/middleware/common/uploadFile.ts
+++ default/middleware/common/uploadFile.ts
@@ -1,18 +1,21 @@
 const { s3, s3_bucket } = require("../../config");
 import * as multer from "multer";
 import * as multerS3 from "multer-s3";
+import * as path from "path";
 
 const uploadFile = multer({
-  storage: multerS3({
-    s3: s3,
-    bucket: s3_bucket,
-    metadata: (req: Express.Request, file: Express.Multer.File, cb) => {
-      cb(null, { fieldName: file.fieldname });
-    },
-    key: (req: Express.Request, file: Express.Multer.File, cb) => {
-      cb(null, Date.now().toString());
-    },
-  }),
+    storage: multerS3({
+        s3: s3,
+        bucket: s3_bucket,
+        metadata: (req: Express.Request, file: Express.Multer.File, cb) => {
+            cb(null, { fieldName: file.fieldname });
+        },
+        key: (req: Express.Request, file: Express.Multer.File, cb) => {
+            const extension = path.extname(file.originalname);
+            cb(null, Date.now().toString() + extension);
+        },
+        acl: "public-read-write",
+    }),
 });
 
 module.exports = uploadFile;
