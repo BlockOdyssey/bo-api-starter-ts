85ffb85cfc2eb59ae79089370f65a411265e1637
blockodyssey-whlee <dokdo2005@blockodyssey.io>
2021-03-18 10:51:40 +0900

    엑셀 파일 생성 함수 작성

--- default/common.ts
+++ default/common.ts
@@ -2,6 +2,7 @@ const { s3, s3_bucket } = require("./config");
 import * as multer from "multer";
 import * as multerS3 from "multer-s3";
 import * as path from "path";
+const xlsx = require("xlsx");
 
 // S3 파일 업로드 (middleware로 붙여서 사용)
 // 파일 하나만 업로드 할 경우 : uploadFile.single(필드명)
@@ -21,6 +22,19 @@ const uploadFile = multer({
     }),
 });
 
+// 엑셀 파일 생성
+const makeExcel = async (data: object, sheetName: string) => {
+    const ws = xlsx.utils.json_to_sheet(data);
+    const wb = xlsx.utils.book_new();
+    xlsx.utils.book_append_sheet(wb, ws, sheetName);
+    const wbOut = xlsx.write(wb, {
+        bookType: "xlsx",
+        type: "buffer",
+    });
+    return wbOut;
+};
+
 module.exports = {
     uploadFile,
+    makeExcel,
 };
